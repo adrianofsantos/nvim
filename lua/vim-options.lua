5008575435250576d19a551fed4a4c0cfc011d6f
Adriano Francisco dos Santos <adrianofsantos@proton.me>
2024-01-24 20:53:34 -0300

    chore: My first commit.

---
+++ lua/vim-options.lua
@@ -0,0 +1,8 @@
+-- indent code with 2 spaces on press tab button
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+-- insert mapleader key
+vim.g.mapleader = " "
+
