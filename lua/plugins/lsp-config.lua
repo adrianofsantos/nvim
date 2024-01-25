47890fcfa8aa0c7cf60e55737c1d1c77b8ed5fc2
Adriano Francisco dos Santos <adrianofsantos@proton.me>
2024-01-25 20:54:22 -0300

    chore: Adicionando plugin alpha e pequenas alterações.

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,25 +1,33 @@
 return {
-  { "williamboman/mason.nvim",
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
     config = function()
-      require('mason').setup()
-    end
+      require("mason").setup()
+    end,
   },
-  { "williamboman/mason-lspconfig.nvim",
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls"}
+        auto_install = true,
       })
-    end
+    end,
   },
-  { "neovim/nvim-lspconfig",
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
     config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
       local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
+      lspconfig.lua_ls.setup({ capabilities = capabilities })
 
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {}) 
-    end
-  }
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
 }
-
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,6 +4,7 @@ return {
 	config = function()
 		local config = require("nvim-treesitter.configs")
 		config.setup({
+      -- Instala novas linguagens de programação automaticamente ao carregar o arquivo
 			auto_install = true,
 			highlight = { enable = true },
 			indent = { enable = true },
